c0e8977567cadd646394358e8c1a0802915ba1c6
Ayush Mishra <ayushm1151@gmail.com>
2025-07-07 18:35:49 +0530

    FastAPI basics completed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, FileResponse
 from pydantic import BaseModel
 from typing import List
-import json
+import json, os
 
 app = FastAPI()
 
@@ -106,3 +106,16 @@ def delete_tea(tea_id: int):
                 'deleted_tea': deleted
             }
     raise HTTPException(status_code=404, detail='Tea not found for the given id.')
+
+
+@app.get('/download_file')
+def download_tea_file():
+
+    if not os.path.exists('database.txt'):
+        raise HTTPException(status_code=404, detail='Database file not found')
+    
+    return FileResponse(
+        path='database.txt',
+        filename='database.txt',
+        media_type='text/plain'
+    )
